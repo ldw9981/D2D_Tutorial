477d33976f85b6321c0cdde3e154966c3b1fbb5b
LeeDongWon <ldw9981@gmail.com>
2024-06-10 23:07:58 +0900

    템플릿 파일 제거

--- D2DCoordinateSystem/D2DCoordinateSystem.cpp
+++ D2DCoordinateSystem/D2DCoordinateSystem.cpp
@@ -1,8 +1,4 @@
-﻿// D2DDrawBitmap.cpp : 애플리케이션에 대한 진입점을 정의합니다.
-//
-
-#include "framework.h"
-#include "D2DCoordinateSystem.h"
+﻿#include <windows.h>
 #include <d2d1.h>
 #include <wincodec.h>
 #include <comdef.h>
@@ -10,13 +6,11 @@
 #include <string>
 #include <dwrite.h>
 
-
 #pragma comment(lib,"d2d1.lib")
 #pragma comment(lib,"dwrite.lib")
 #pragma comment(lib,"dxgi.lib")
 #pragma comment(lib,"windowscodecs.lib")
 
-#define MAX_LOADSTRING 100
 
 using namespace D2D1;
 using namespace std;
@@ -24,8 +18,6 @@ using namespace std;
 // 전역 변수:
 HINSTANCE g_hInst;                                // 현재 인스턴스입니다.
 HWND g_hWnd;
-WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
-WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
 
 // 윌드행렬(복합변환)을 위한 행렬 
 D2D_MATRIX_3X2_F g_matWorldScale = Matrix3x2F::Identity(); 
@@ -52,11 +44,8 @@ IDWriteTextFormat* g_pDWriteTextFormat;
 
 ID2D1SolidColorBrush* g_pBrush; // 브러시 개체 인터페이스 포인터 변수
 
-// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
 
 HRESULT CreateD2DBitmapFromFile(const WCHAR* szFilePath, ID2D1Bitmap** ppID2D1Bitmap)
 {
@@ -246,28 +235,54 @@ void UninitDirect2D()
 }
 
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-    _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPWSTR    lpCmdLine,
-    _In_ int       nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	// 윈도우 클래스 구조체 초기화
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;	// 윈도우 프로시저 함수
+	wc.hInstance = hInstance;   // 인스턴스 핸들
+	// NULL을 사용하면 현재 실행 중인 프로그램의 인스턴스 핸들을 사용하게 됩니다.두 번째 인자는 로드할 커서의 ID입니다.
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.lpszClassName = L"BasicWindowClass";
 
-    // TODO: 여기에 코드를 입력합니다.
+	// 윈도우 클래스 등록
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, L"윈도우 클래스 등록 실패", L"에러", MB_OK | MB_ICONERROR);
+		return 1;
+	}
 
-    // 전역 문자열을 초기화합니다.
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_D2DCOORDINATESYSTEM, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	// 원하는 크기가 조정되어 리턴
+	SIZE clientSize = { 1920, 1280 };
+	RECT clientRect = { 0, 0, clientSize.cx, clientSize.cy };
+	AdjustWindowRect(&clientRect, WS_OVERLAPPEDWINDOW, FALSE);
 
-    // 애플리케이션 초기화를 수행합니다:
-    if (!InitInstance(hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+	// 윈도우 생성
+	g_hWnd = CreateWindowEx(
+		0,
+		L"BasicWindowClass",
+		L"간단한 윈도우",
+		WS_OVERLAPPEDWINDOW,	// OR연산으로 조합된 윈도우창 스타일
+		0, 0,	// 시작위치
+		clientRect.right - clientRect.left, clientRect.bottom - clientRect.top, // 너비, 높이
+		NULL, NULL, hInstance, NULL
+	);
 
-    InitDirect2D();
+	if (!g_hWnd)
+	{
+		MessageBox(NULL, L"윈도우 생성 실패", L"에러", MB_OK | MB_ICONERROR);
+		return 1;
+	}
+
+	// 윈도우 표시
+	ShowWindow(g_hWnd, nCmdShow);
+	UpdateWindow(g_hWnd);
+
+	if (!InitDirect2D())
+		return FALSE;
 
 
     MSG msg;
@@ -319,81 +334,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     return (int)msg.wParam;
 }
 
-
-
-//
-//  함수: MyRegisterClass()
-//
-//  용도: 창 클래스를 등록합니다.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc = WndProc;
-    wcex.cbClsExtra = 0;
-    wcex.cbWndExtra = 0;
-    wcex.hInstance = hInstance;
-    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_D2DCOORDINATESYSTEM));
-    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wcex.lpszMenuName = NULL;
-    wcex.lpszClassName = szWindowClass;
-    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
-}
-
-//
-//   함수: InitInstance(HINSTANCE, int)
-//
-//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
-//
-//   주석:
-//
-//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
-//        주 프로그램 창을 만든 다음 표시합니다.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-    g_hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
-
-	// 원하는 크기가 조정되어 리턴
-	RECT rcClient = { 0, 0, (LONG)1024, (LONG)768 };
-	AdjustWindowRect(&rcClient, WS_OVERLAPPEDWINDOW, FALSE);
-
-	//생성
-    g_hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-		100, 100,	// 시작 위치
-		rcClient.right - rcClient.left, rcClient.bottom - rcClient.top,
-		nullptr, nullptr, hInstance, nullptr);
-
-    if (!g_hWnd)
-    {
-        return FALSE;
-    }
-
-    ShowWindow(g_hWnd, nCmdShow);
-    UpdateWindow(g_hWnd);
-
-    SetWindowText(g_hWnd,L"화살표 키로 카메라 이동합니다 ");
-
-    return TRUE;
-}
-
-//
-//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  용도: 주 창의 메시지를 처리합니다.
-//
-//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
-//  WM_PAINT    - 주 창을 그립니다.
-//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
-//
-//
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     std::wstring str;
@@ -421,31 +361,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         str = L"Camera X : " + to_wstring(g_posCamera.x) + L" Camera Y : " + std::to_wstring(g_posCamera.y);
         SetWindowText(hWnd, str.c_str());
 		break;
-    case WM_COMMAND:
-    {
-        int wmId = LOWORD(wParam);
-        // 메뉴 선택을 구문 분석합니다:
-        switch (wmId)
-        {
-        case IDM_ABOUT:
-            DialogBox(g_hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-            break;
-        case IDM_EXIT:
-            DestroyWindow(hWnd);
-            break;
-        default:
-            return DefWindowProc(hWnd, message, wParam, lParam);
-        }
-    }
-    break;
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hWnd, &ps);
-        // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
-        EndPaint(hWnd, &ps);
-    }
-    break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -455,23 +370,3 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-// 정보 대화 상자의 메시지 처리기입니다.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
-}
