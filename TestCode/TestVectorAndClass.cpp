d290dc4a5215cc1c05f85441c8a025ae088bc7f5
LeeDongWon <ldw9981@gmail.com>
2024-06-27 12:59:16 +0900

    update

--- TestCode/TestVectorAndClass.cpp
+++ TestCode/TestVectorAndClass.cpp
@@ -1,24 +1,35 @@
 #include "TestVectorAndClass.h"
 #include <vector>
 #include <iostream>
-
-
+#include <stdio.h>
+#include <assert.h>
 
 class Test
 {
 public:
+    Test* pInstance = nullptr;
     static int ConstructCount;
     static int DestructCount;
     Test()
     {
+        pInstance = this;
         ConstructCount++;
-        std::cout << "생성자 호출됨" << ConstructCount << std::endl;
+        std::cout << "생성자 호출됨" << this << ConstructCount << std::endl;
        
+    }
+    Test(const Test& other)
+    {
+        pInstance = this;
+        ConstructCount++;
+        std::cout << "복사 생성자 호출됨" << this << ConstructCount << std::endl;
     }
     ~Test()
     {
         DestructCount++;
-        std::cout << "파괴자 호출됨" << DestructCount << std::endl;
+        std::cout << "파괴자 호출됨" << this;
+        assert(pInstance == this);
+
+        std::cout << DestructCount << std::endl;
         
     }
 };
@@ -30,8 +41,16 @@ void mainTestVectorAndClass()
     std::vector<Test> Container;
     Test item;
 
+    std::cout << "Container.push_back(item)" << std::endl;
+    Container.push_back(item);   
+    Container.push_back(item);
+    Container.push_back(item);
+    Container.push_back(item);
+    Container.push_back(item);
+    Container.push_back(item);
 
-    Container.push_back(item);
-    Container.push_back(item);
-    Container.push_back(item);
+
+
+    std::cout << "키를눌러야 진행" << std::endl;
+    system("pause");
 }
